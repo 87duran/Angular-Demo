11a513770fd7dad81944df0ffae77c99aeae373d
87duran <87duran@gmail.com>
2014-10-07 19:20:09 -0600

    first commit

---
+++ MainCtrl.js
@@ -0,0 +1,8 @@
+var app = angular.module('ngDemo');
+
+app.controller('MainCtrl', function($scope){
+	$scope.test = 'Hello World';
+	$scope.devStudents = ['Ryan', 'Kelsey', 'Tom', 'Josh', 'Ron', 'Rick'];
+
+
+});
