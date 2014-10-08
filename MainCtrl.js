78533498cb1b38c51a7edc65516ac63059805b03
87duran <87duran@gmail.com>
2014-10-08 17:49:41 -0600

    2nd commit

--- MainCtrl.js
+++ MainCtrl.js
@@ -3,6 +3,9 @@ var app = angular.module('ngDemo');
 app.controller('MainCtrl', function($scope){
 	$scope.test = 'Hello World';
 	$scope.devStudents = ['Ryan', 'Kelsey', 'Tom', 'Josh', 'Ron', 'Rick'];
+	$scope.toggleSearch = function(){
+		$scope.showSearch = !$scope.showSearch;
+	}
 
 
 });
